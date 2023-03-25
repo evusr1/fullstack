967d18f88db2b5fa2f63a59964e992f3f8344ead
evusr1 <127464885+evusr1@users.noreply.github.com>
2023-03-24 20:24:08 -0700

    ex4.1-4.2; make blog and refactor code

---
+++ part4/bloglist/app.js
@@ -0,0 +1,16 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogRouter = require('./controllers/blog')
+
+mongoose.connect(config.mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
